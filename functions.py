8223578fcbbe3afd2094337f557c3ca2a53564bd
Dann Starr <dannstarr@gmail.com>
2025-02-13 21:03:21 +0000

    Create basic GUI without interactions. Day 16

--- functions.py
+++ functions.py
@@ -6,3 +6,8 @@ def get_todos(filepath):
 def write_todos(todos_arg, filepath="files/todos.txt"):
     with open(filepath, "w") as file:
         todos_local = file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
---
+++ gui.py
@@ -0,0 +1,10 @@
+import FreeSimpleGUI as sg
+import functions
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter to-do")
+add_button = sg.Button("Add")
+
+window = sg.Window('My To-Do App', layout=[[label, input_box, add_button]])
+window.read()
+window.close()
