1e5d7b55891bd8001427e06f2aae6f1071c7129b
Dann Starr <dannstarr@gmail.com>
2025-02-14 22:19:17 +0000

    Implement the add todo feature. Day 17

--- functions.py
+++ functions.py
@@ -1,9 +1,11 @@
-def get_todos(filepath):
+FILEPATH = "files/todos.txt"
+
+def get_todos(filepath = FILEPATH):
     with open(filepath, "r") as file:
         todos_local = file.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath="files/todos.txt"):
+def write_todos(todos_arg, filepath = FILEPATH):
     with open(filepath, "w") as file:
         todos_local = file.writelines(todos_arg)
 
